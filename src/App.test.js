5349d5936d208889088c983342458a7e66b091a5
Dar-ko <darko.velickovic1@gmail.com>
2023-10-18 20:59:18 +0200

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+// Check if the component renders without errors
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/SpaceX Rockets/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+// Check if the search input works
+test('search input works', () => {
+  render(<App />);
+  const searchInput = screen.getByPlaceholderText('Search Rockets');
+
+  // Wrap the user interaction in act
+  act(() => {
+    userEvent.type(searchInput, 'Falcon');
+  });
+
+  // Check if the input value is updated
+  expect(searchInput).toHaveValue('Falcon');
+
+  // Use a more flexible text matcher for rocket name
+  const rocketNameElement = screen.getByText(/Falcon/i, { exact: false });
+  expect(rocketNameElement).toBeInTheDocument();
 });
